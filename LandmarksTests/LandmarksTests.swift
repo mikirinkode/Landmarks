e5c357051df506bd0326eeae4ecfd13662b4dd62
mikirinkode <muhammadwafa105@gmail.com>
2024-11-12 20:36:00 +0700

    Initial Commit

---
+++ LandmarksTests/LandmarksTests.swift
@@ -0,0 +1,16 @@
+//
+//  LandmarksTests.swift
+//  LandmarksTests
+//
+//  Created by MacBook on 12/11/24.
+//
+
+import Testing
+
+struct LandmarksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
