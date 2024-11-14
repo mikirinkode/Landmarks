70e48521d149cec891fd6a19f5694ed933cbb9db
mikirinkode <muhammadwafa105@gmail.com>
2024-11-14 09:35:53 +0700

    Finish SwiftUI Tutorial: - Creating and combining views - Building list and navigation

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by MacBook on 12/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Landmarks/Views/MapView.swift
@@ -0,0 +1,26 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by MacBook on 12/11/24.
+//
+
+import SwiftUI
+import MapKit
+struct MapView: View {
+    var coordinate: CLLocationCoordinate2D
+    
+    var body: some View {
+        Map(position: .constant(.region(region)))
+    }
+    private var region: MKCoordinateRegion {
+        MKCoordinateRegion(
+            center: coordinate,
+            span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        )
+    }
+}
+
+#Preview {
+    MapView(coordinate: landmarks[0].locationCoordinate)
+}
