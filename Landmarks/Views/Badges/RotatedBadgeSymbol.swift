e62a450c160d549f28cf09f158b5aecf47ef99d2
mikirinkode <muhammadwafa105@gmail.com>
2024-11-15 10:07:00 +0700

    Drawing paths and shapes feat: add custom badge

---
+++ Landmarks/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,22 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by MacBook on 15/11/24.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+#Preview {
+    RotatedBadgeSymbol(angle: Angle(degrees: 0))
+}
